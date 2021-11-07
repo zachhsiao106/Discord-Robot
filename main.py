45bdce79a579eb4949303107fc7a46f4aed57a71
zachhsiao106 <92644014+zachhsiao106@users.noreply.github.com>
2021-11-07 15:46:56 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello")
+import discord
+from discord.ext import commands
+from discord.ext.commands.core import commands
+
+bot = commands.Bot(command_prefix= '/')
+
+@bot.event
+async def Fucname(parameter_list):
+    pass
