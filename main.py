83666c6c5f21439bd28819cc2ea0115c9025e988
zachhsiao106 <92644014+zachhsiao106@users.noreply.github.com>
2021-11-13 21:36:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,4 +18,4 @@ async def on_member_remove(member):
     channel = bot.get_channel(877085946263265313)
     await channel.send(f'{member} leave!')
 
-bot.run('ODk4OTc4MDc4NDAxODk2NTY5.YWsElA.5CIB5gqF6WzQcyFISyOvsM25b_g')
+bot.run('')
