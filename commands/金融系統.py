bf87593ef20194977694143038b2795a541e6a9c
zachhsiao106 <92644014+zachhsiao106@users.noreply.github.com>
2022-04-10 14:03:57 +0800

    20220410

--- commands/金融系統.py
+++ commands/金融系統.py
@@ -87,7 +87,7 @@ class 金融系統(Cog_Extension):
             await ctx.send(f'{ctx.author.mention} 你沒有申請銀行帳戶!(申請帳戶請在伺服器裡打「~sign_up_bank_account」)!')
         
     @commands.command()
-    async def give_money(self, ctx, money:int, *, bank_account_name):
+    async def give_money(self, ctx, money, *, bank_account_name):
         bank_account_id = []
         bank_account = []
         bank_money = []
@@ -99,35 +99,42 @@ class 金融系統(Cog_Extension):
 
         if ctx.author.id in bank_account_id:
             if bank_account_name in bank_account:
-                if money > 0:
-                    if ctx.author.name != bank_account_name:
-                        if bank_money[bank_account_id.index(ctx.author.id)] >= money:
-                            bank_account[bank_account_id.index(ctx.author.id)] = ctx.author.name
-                            bank_money[bank_account_id.index(ctx.author.id)] = bank_money[bank_account_id.index(ctx.author.id)] - money
-                            bank_money[bank_account.index(bank_account_name)] = bank_money[bank_account.index(bank_account_name)] + money
-                            jdata['bank_account'] = bank_account
-                            jdata['bank_money'] = bank_money
-                            with open('data.json','w',encoding='utf8') as jfile:
-                                json.dump(jdata,jfile,indent=4)
-                            await ctx.send(f'{ctx.author.mention} 成功轉帳給 {bank_account_name} {money} 元!')
+                if type(eval(money)) == int:
+                    if int(money) > 0:
+                        if ctx.author.name != bank_account_name:
+                            if bank_money[bank_account_id.index(ctx.author.id)] >= int(money):
+                                bank_account[bank_account_id.index(ctx.author.id)] = ctx.author.name
+                                bank_money[bank_account_id.index(ctx.author.id)] = bank_money[bank_account_id.index(ctx.author.id)] - int(money)
+                                bank_money[bank_account.index(bank_account_name)] = bank_money[bank_account.index(bank_account_name)] + int(money)
+                                jdata['bank_account'] = bank_account
+                                jdata['bank_money'] = bank_money
+                                with open('data.json','w',encoding='utf8') as jfile:
+                                    json.dump(jdata,jfile,indent=4)
+                                await ctx.send(f'{ctx.author.mention} 成功轉帳給 {bank_account_name} {money} 元!')
+                            else:
+                                bank_account[bank_account_id.index(ctx.author.id)] = ctx.author.name
+                                jdata['bank_account'] = bank_account
+                                with open('data.json','w',encoding='utf8') as jfile:
+                                    json.dump(jdata,jfile,indent=4)
+                                await ctx.send(f'{ctx.author.mention} 你的餘額不足!')
                         else:
                             bank_account[bank_account_id.index(ctx.author.id)] = ctx.author.name
                             jdata['bank_account'] = bank_account
                             with open('data.json','w',encoding='utf8') as jfile:
                                 json.dump(jdata,jfile,indent=4)
-                            await ctx.send(f'{ctx.author.mention} 你的餘額不足!')
+                            await ctx.send(f'{ctx.author.mention} 你耍了你自己!等一下,你做不到!(請不要給自己錢!)')
                     else:
                         bank_account[bank_account_id.index(ctx.author.id)] = ctx.author.name
-                        jdata['bank_account'] = bank_account
                         with open('data.json','w',encoding='utf8') as jfile:
+                            jdata['bank_account'] = bank_account
                             json.dump(jdata,jfile,indent=4)
-                        await ctx.send(f'{ctx.author.mention} 你耍了你自己!等一下,你做不到!(請不要給自己錢!)')
+                        await ctx.send(f'{ctx.author.mention} 你耍了你自己!等一下,你做不到!(請不要給0元!)')
                 else:
                     bank_account[bank_account_id.index(ctx.author.id)] = ctx.author.name
+                    jdata['bank_account'] = bank_account
                     with open('data.json','w',encoding='utf8') as jfile:
-                        jdata['bank_account'] = bank_account
                         json.dump(jdata,jfile,indent=4)
-                    await ctx.send(f'{ctx.author.mention} 你耍了你自己!等一下,你做不到!(請不要給0元!)')
+                    await ctx.send(f'{ctx.author.mention} 請給整數的金額!')
             else:
                 bank_account[bank_account_id.index(ctx.author.id)] = ctx.author.name
                 jdata['bank_account'] = bank_account
@@ -169,6 +176,40 @@ class 金融系統(Cog_Extension):
                 await ctx.send(f'{ctx.author.mention} 請等到明天領取!')
         else:
             await ctx.send(f'{ctx.author.mention} 你沒有申請銀行帳戶!(申請帳戶請在伺服器裡打「~sign_up_bank_account」)!')
+
+    @commands.command()
+    async def check_money_leader(self, ctx):
+        leader = ""
+        money = ""
+        No = ""
+        temp_str = ""
+        temp_int = 0
+        bank_account_id = []
+        bank_account = []
+        bank_money = []
+        with open('data.json','r',encoding='utf8') as jfile:
+            jdata = json.load(jfile)
+        bank_account_id = jdata['bank_account_id']
+        bank_account = jdata['bank_account']
+        bank_money = jdata['bank_money']
+        for i in range(len(bank_money)):
+            for j in range(len(bank_money) - 1):
+                if bank_money[j] <= bank_money[j + 1]:
+                    temp_int = bank_money[j]
+                    bank_money[j] = bank_money[j + 1]
+                    bank_money[j + 1] = temp_int
+                    temp_str = bank_account[j]
+                    bank_account[j] = bank_account[j + 1]
+                    bank_account[j + 1] = temp_str
+        for k in range(len(bank_money)):
+            No += f'No.{k + 1}\n'
+            leader += f'{bank_account[k]}\n'
+            money += f'{bank_money[k]} 元\n'
+        embed=discord.Embed(title="金融排行榜", color=0xdfaa70)
+        embed.add_field(name="名次",value=No, inline=True)
+        embed.add_field(name="名稱",value=leader, inline=True)
+        embed.add_field(name="銀行存款", value=money, inline=True)
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(金融系統(bot))
